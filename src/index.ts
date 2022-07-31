a8a3b5b7bb40f1a83a53a2eb85277ffe66a62947
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-07-31 16:03:06 -0600

    continuing to build out the toDo list typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,37 @@
-const list = document.getElementById("list") as HTMLUListElement
-const form = document.getElementById("new-task form") as HTMLFormElement
-const input = document.getElementById("new-task-title") as HTMLInputElement
+import { v4 as uuidV4 } from "uuid"
+
+type Task = {
+  id: String,
+  title: String,
+  completed: Boolean,
+  createdAt: Date
+}
+
+const list = document.getElementById("list") as HTMLUListElement || null
+const form = document.getElementById("new-task form") as HTMLFormElement || null
+const input = document.getElementById("new-task-title") as HTMLInputElement || null
+
+form?.addEventListener("submit", event => {
+  event.preventDefault()
+
+  if (input.value == "" || input.value == null) {
+    return
+  }
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  }
+
+  addListItem(newTask)
+})
+
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+
+}
