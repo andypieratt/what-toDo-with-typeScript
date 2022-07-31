a10923cc8fbda329f3312832edd50bbb63d1fb9a
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-07-31 15:48:07 -0600

    started setting up some typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.getElementById("list") as HTMLUListElement
+const form = document.getElementById("new-task form") as HTMLFormElement
+const input = document.getElementById("new-task-title") as HTMLInputElement
